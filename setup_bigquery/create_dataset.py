c78c23fcec8ce9670ae220338507be37fed684ca
ymd65536 <ymd65536@gmail.com>
2024-03-27 15:23:49 +0900

    add: create_dataset

--- setup_bigquery/create_dataset.py
+++ setup_bigquery/create_dataset.py
@@ -1,13 +1,15 @@
 from google.cloud import bigquery
 
-# Construct a BigQuery client object.
-client = bigquery.Client(location="asia-northeast1", project="project-id")
-dataset_id = "linebot2"
-dataset = bigquery.Dataset(dataset_id)
+project_id = "project_id"
+client = bigquery.Client(location="asia-northeast1", project=project_id)
 
+dataset_id = f"{project_id}.linebot2"
+dataset = bigquery.Dataset(dataset_id)
 dataset = client.create_dataset(dataset, timeout=30)
-print("Created dataset {}.{}".format(client.project, dataset.dataset_id))
 
 table_ref = dataset.table("data-table")
 table = bigquery.Table(table_ref)
 table = client.create_table(table)
+
+print(f"Created dataset {client.project}.{dataset.dataset_id}")
+print(f"Table Name:{table.table_id}")
